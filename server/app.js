aa00e831c58cce15fffd189df511faf8d97feb78
nergal-perm <nergal@psu.ru>
2017-01-13 10:13:13 +0500

    Finished boilerplating the project, added environment-specific configs

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const config =  require('config');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+}
+
+app.get('/api/welcome', (req, res) => {
+    res.json([{
+    	envVariable: process.env.NODE_ENV,
+    	path: config.get('localFilePath')
+    }]);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
