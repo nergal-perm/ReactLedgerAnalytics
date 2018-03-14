704c339c2f3de286adbed96a9a0a6b4fc47dc64b
Eugene Terekhov <nergal@psu.ru>
2018-03-14 09:39:02 +0500

    Add ESLint, started TDD-ing LedgerWrapper class, WIP

--- server/tests/ledger-wrapper-test.js
+++ server/tests/ledger-wrapper-test.js
@@ -7,22 +7,25 @@ const LedgerWrapper = require('../utils/LedgerWrapper');
 describe('LedgerWrapper', function() {
     beforeEach(function() {
         this.ledger = new LedgerWrapper();
-        
-        // stubbing getBalance() method because it's going to make I/O calls
-        this.getBalanceStub = sinon.stub(this.ledger, 'getBalance');
-        this.getBalanceStub.returns(10);
     });
 
     afterEach(function() {
-        this.getBalanceStub.restore();
         this.ledger = null;
     });
 
-    it('should be properly initialized', function() {
-        expect(this.ledger.getDescription()).to.equal("Hello");
+    it('should be empty if options object is empty', function() {
+        expect(this.ledger.commandLine.join(' ')).to.equal('');
     });
 
-    it('should return balance (mocked)', function() {
-        expect(this.getBalanceStub()).to.equal(10);
-    })
+    it('should be properly initialized with valid options object', function() {
+        const options = {
+            file: 'ledger.txt',
+            reportType: 'register',
+            reportCurrency: 'руб',
+
+        }
+        this.ledger.setOptions(options);
+        expect(this.ledger.commandLine.join(' ')).to.equal('-f ledger.txt register -X руб');
+    });
+
 });
--- server/utils/LedgerWrapper.js
+++ server/utils/LedgerWrapper.js
@@ -1,11 +1,56 @@
-function LedgerWrapper() {}
+/**
+ * Wrapper for the command-line utility `Ledger`. 
+ * Should be initialized with options object that
+ * turns into command-line arguments
+ */
+class LedgerWrapper {
+  /**
+   * Create a wrapper object, parsing options object
+   * @param {Object} options 
+   */
+  constructor(options) {
+    if(options != null) {
+      this.parseOptions(options);
+    }
+  }
 
-LedgerWrapper.prototype.getDescription = function() {
-    return "Hello";
-};
+  /**
+   * Updates current options with a new set
+   * @param {Object} options 
+   */
+  setOptions(options) {
+    this.parseOptions(options);
+  }
 
-LedgerWrapper.prototype.getBalance = function() {
+  /**
+   * @return {Array} Command line arguments, properly escaped and quoted
+   */
+  get commandLine() {
+    let cliArguments = []
+      .concat(this.file != null ? this.file : [])
+      .concat(this.reportType != null ? this.reportType : [])
+      .concat(this.reportCurrency != null ? this.reportCurrency : []);
+    return cliArguments;
+  }
 
-};
+  /**
+   * Converts options into instance fields, properly escaping and 
+   * quoting those command line arguments
+   * @param {Object} options 
+   */
+  parseOptions(options) {
+    if (options.file != null) {
+      // TODO: Validate file path
+      this.file = ['-f', options.file];
+    }
+    if (options.reportType != null) {
+      // TODO: Validate reportType
+      this.reportType = [options.reportType]
+    }
+    if (options.reportCurrency != null) {
+      this.reportCurrency = ['-X', options.reportCurrency];
+    }
+  }
+}
 
 module.exports = LedgerWrapper;
