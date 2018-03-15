d7e7558972255ac2866d4f95312bc2a45177047b
nergal-perm <nergal@psu.ru>
2018-03-15 08:41:14 +0500

    Refactored tests for LedgerWrapper, added report type validation

--- server/tests/ledger-wrapper-test.js
+++ server/tests/ledger-wrapper-test.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 const chai = require('chai');
 const sinon = require('sinon');
 const expect = chai.expect;
 
 const LedgerWrapper = require('../utils/LedgerWrapper');
 
-describe('LedgerWrapper', function() {
+describe('LedgerWrapper general tests', function() {
     beforeEach(function() {
         this.ledger = new LedgerWrapper();
     });
@@ -17,15 +18,73 @@ describe('LedgerWrapper', function() {
         expect(this.ledger.commandLine.join(' ')).to.equal('');
     });
 
-    it('should be properly initialized with valid options object', function() {
-        const options = {
-            file: 'ledger.txt',
-            reportType: 'register',
-            reportCurrency: 'руб',
+    it('should validate report types', function() {
+        const validReportTypes = ['register', 'balance'];
+        validReportTypes.forEach(reportType => {
+            expect(LedgerWrapper.validateReportType(reportType)).to.true;
+        });
 
-        }
-        this.ledger.setOptions(options);
-        expect(this.ledger.commandLine.join(' ')).to.equal('-f ledger.txt register -X руб');
+        const invalidReportTypes = ['report', 'general'];
+        invalidReportTypes.forEach(reportType => {
+            expect(LedgerWrapper.validateReportType(reportType)).to.false;
+        });
+    });
+});
+
+describe('LedgerWrapper specific arguments', function() {
+    beforeEach(function() {
+        this.ledger = new LedgerWrapper();
     });
 
+    afterEach(function() {
+        this.ledger = null;
+    });
+
+    it('should set up file argument or mark Wrapper as invalid', function() {
+        const fileNames = ['ledger.txt', '/path/to/ledger.file', null];
+        fileNames.forEach(fileName => {
+            let expected = '-f ' + fileName;
+            this.ledger.setOptions({
+                file: fileName,
+                reportType: 'register'      // valid report type is not optional
+            });
+            if (fileName != null) {
+                expect(this.ledger.isValid).to.true;
+                expect(this.ledger.commandLine.join(' ').includes(expected)).to.true;
+            } else {
+                expect(this.ledger.isValid).to.false;
+            }
+        });
+    });
+
+    it('should set up report type of mark Wrapper as invalid', function() {
+        const reportTypes = ['register', 'balance', null];
+        reportTypes.forEach(reportType => {
+            this.ledger.setOptions({
+                reportType: reportType,
+                file: 'some_file'           // file name is not optional
+            });
+            if (reportType != null) {
+                expect(this.ledger.isValid).to.true;
+                expect(this.ledger.commandLine.includes(reportType)).to.true;
+            } else {
+                expect(this.ledger.isValid).to.false;
+            }
+        });
+    });
+
+    it('should set up report currency', function() {
+        const currencies = [null, 'USD', 'руб'];
+        currencies.forEach(currency => {
+            let expected = '-X ' + (currency == null ? '' : currency);
+            this.ledger.setOptions({
+                reportCurrency: currency
+            });
+            if (currency == null) {
+                expect(this.ledger.commandLine.join(' ').includes(expected.trim())).to.false;
+            } else {
+                expect(this.ledger.commandLine.join(' ').includes(expected)).to.true;
+            }
+        });
+    });
 });
--- server/utils/LedgerWrapper.js
+++ server/utils/LedgerWrapper.js
@@ -1,32 +1,39 @@
 /**
- * Wrapper for the command-line utility `Ledger`. 
+ * Wrapper for the command-line utility `Ledger`.
  * Should be initialized with options object that
  * turns into command-line arguments
  */
 class LedgerWrapper {
   /**
    * Create a wrapper object, parsing options object
-   * @param {Object} options 
+   * @param {Object} options
    */
   constructor(options) {
-    if(options != null) {
+    if (options != null) {
       this.parseOptions(options);
     }
   }
 
   /**
    * Updates current options with a new set
-   * @param {Object} options 
+   * @param {Object} options
    */
   setOptions(options) {
     this.parseOptions(options);
   }
 
   /**
+   * @return {Boolean} Whether all passed options were valid
+   */
+  get isValid() {
+    return this.valid;
+  }
+
+  /**
    * @return {Array} Command line arguments, properly escaped and quoted
    */
   get commandLine() {
-    let cliArguments = []
+    const cliArguments = []
       .concat(this.file != null ? this.file : [])
       .concat(this.reportType != null ? this.reportType : [])
       .concat(this.reportCurrency != null ? this.reportCurrency : []);
@@ -34,22 +41,46 @@ class LedgerWrapper {
   }
 
   /**
-   * Converts options into instance fields, properly escaping and 
+   * Converts options into instance fields, properly escaping and
    * quoting those command line arguments
-   * @param {Object} options 
+   * @param {Object} options
    */
   parseOptions(options) {
     if (options.file != null) {
-      // TODO: Validate file path
       this.file = ['-f', options.file];
+    } else {
+      this.valid = false;
     }
-    if (options.reportType != null) {
-      // TODO: Validate reportType
-      this.reportType = [options.reportType]
+    if (options.reportType != null &&
+        LedgerWrapper.validateReportType(options.reportType)) {
+      this.reportType = [options.reportType];
+    } else {
+      this.valid = false;
     }
     if (options.reportCurrency != null) {
       this.reportCurrency = ['-X', options.reportCurrency];
     }
+
+    // should be at the very end
+    if (this.valid !== false) {
+      this.valid = true;
+    }
+  }
+
+  /**
+   * @return {Array} Valid report types
+   */
+  static validReportTypes() {
+    return ['register', 'balance'];
+  }
+
+  /**
+   * Checks if specified report type is supported by ledger-cli
+   * @param {String} reportType
+   * @return {Boolean} whether reportType is supported
+   */
+  static validateReportType(reportType) {
+    return LedgerWrapper.validReportTypes().includes(reportType);
   }
 }
 
